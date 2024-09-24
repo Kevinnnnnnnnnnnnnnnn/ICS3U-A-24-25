a1fbecc887feacd21b9bd4c0f70e4c0a2e3456b5
Kevin Wang <hankai.kevin.w@outlook.com>
2024-09-24 16:24:42 -0400

    revision before small test

---
+++ Unit One - Introduction to Java/src/day4/ExampleOne.java
@@ -0,0 +1,14 @@
+package day4;
+
+public class ExampleOne {
+    public static void main(String[] args) {
+        // System.out.println("Hello");
+
+        int x = 4;
+        int y = 7;
+        // int sum = x + y;
+
+        System.out.println(x + " + " + y + " = " + (x + y));
+        // string concatenation
+    }
+}
