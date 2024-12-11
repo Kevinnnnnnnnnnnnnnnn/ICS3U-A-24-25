2c3ec68738fd3592a687592770e1d643d6279bcb
Kevin Wang <hankai.kevin.w@outlook.com>
2024-12-11 09:51:01 -0500

    incomplete classes practice

---
+++ Unit Five - Creating Classes/src/day2/Scoreboard.java
@@ -0,0 +1,15 @@
+package day2;
+
+public class Scoreboard {
+    private String team1;
+    private String team2;
+
+    public Scoreboard(String team1, String team2) {
+        this.team1 = team1;
+        this.team2 = team2;
+    }
+
+    public void recordPlay(int points) {
+        
+    }
+}
