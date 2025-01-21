ffee4217acb33919b45205ab0ba0da06642f7b06
Kevin Wang <hankai.kevin.w@outlook.com>
2025-01-21 08:48:36 -0500

    Candy classes

---
+++ Unit Six - Arrays/src/day4/BoxOfCandy.java
@@ -0,0 +1,13 @@
+package day4;
+
+public class BoxOfCandy {
+    private Candy[][] box;
+
+    public boolean moveCandyToFirstRow (int col) {
+        return true;
+    }
+
+    public Candy removeNextByFlavour (String flavour) {
+        return null;
+    }
+}
---
+++ Unit Six - Arrays/src/day4/Candy.java
@@ -0,0 +1,13 @@
+package day4;
+
+public class Candy {
+    private String flavour;
+
+    public Candy(String flavour){
+        this.flavour = flavour;
+    }
+
+    public String getFlavour(){
+        return "";
+    }
+}
